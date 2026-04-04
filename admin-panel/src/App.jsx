b4e262c990a6686ce929222804b93db151a28dc5
Rosário <rayssarosario27@gmail.com>
2026-04-04 16:50:12 -0300

    feat: Protect admin routes with authentication requirement

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Layout from './components/Layout'
+import ProtectedRoute from './components/ProtectedRoute'
 import Dashboard from './pages/Dashboard'
 import Apresentacoes from './pages/Apresentacoes'
 import Datas from './pages/Datas'
@@ -12,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Layout />}>
+        <Route path="/admin" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="apresentacoes" element={<Apresentacoes />} />
           <Route path="apresentacoes/:id" element={<EditarApresentacao />} />
